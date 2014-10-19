41e93c33e3f3753fc402973f83b5de76340445eb
Rob Finean <rfinean@iee.org>
2014-10-19 01:18:16 +0100

    display dummy local data

---
+++ prediction.js
@@ -0,0 +1,67 @@
+var data = [
+{
+rain: 0,
+prob: 1,
+time: "12:00:00"
+},
+{
+rain: 0,
+prob: 1,
+time: "12:05:00"
+},
+{
+rain: 0,
+prob: 1,
+time: "12:10:00"
+},
+{
+rain: 1,
+prob: 0.25,
+time: "12:15:00"
+},
+{
+rain: 4,
+prob: 0.5,
+time: "12:20:00"
+},
+{
+rain: 7,
+prob: 0.8,
+time: "12:25:00"
+},
+{
+rain: 10,
+prob: 1,
+time: "12:30:00"
+},
+{
+rain: 10,
+prob: 1,
+time: "12:35:00"
+},
+{
+rain: 5,
+prob: 0.9,
+time: "12:40:00"
+},
+{
+rain: 2,
+prob: 0.4,
+time: "12:45:00"
+},
+{
+rain: 0,
+prob: 1,
+time: "12:50:00"
+},
+{
+rain: 0,
+prob: 1,
+time: "12:55:00"
+},
+{
+rain: 0,
+prob: 1,
+time: "13:00:00"
+}
+];
